2703fa3cb3bdd19420b7e9bd3a83018155a7dd77
Sumit Zarkhande <sumitzarkhande@gmail.com>
2026-08-29 12:59:28 +0530

    Kubernetes-python_application

---
+++ Kubernetes-project/app/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Kubernetes Real World Project - Application Running"
+
+@app.route("/health")
+def health():
+    return "Healthy", 200
+
+@app.route("/config")
+def config():
+    return f"Environment: {os.getenv('APP_ENV', 'not-configured')}"
+
+@app.route("/secret")
+def secret():
+    if os.getenv("APP_SECRET"):
+        return "Secret is configured"
+    return "Secret is NOT configured"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
